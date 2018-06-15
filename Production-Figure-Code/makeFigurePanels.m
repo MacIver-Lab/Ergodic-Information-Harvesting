6b4de019c14202ba97d9ba0edb96675660bee4bf
Chen Chen <chenchen.bme@gmail.com>
2018-06-14 21:53:40 -0500

    update figure code to make compatible with MATLAB R2017a

--- Production-Figure-Code/makeFigurePanels.m
+++ Production-Figure-Code/makeFigurePanels.m
@@ -32,7 +32,7 @@ warning('off', 'MATLAB:MKDIR:DirectoryExists');
 %                   does not require any simulation data and therefore
 %                   USE_PUBLISHED_DATASET will be ignored
 % 
-targetFig = 'sm-fig3';
+targetFig = 'fig2';
 
 % Maximum number of CPU thread dedicated for sm-fig4 simulation
 % Note that this is only used for sm-fig4 and the number will automatically
