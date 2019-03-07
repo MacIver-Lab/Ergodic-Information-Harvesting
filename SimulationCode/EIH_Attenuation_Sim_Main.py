e32be735a818887ce4e19f2c7fe339b57ea248e7
Chen Chen <chenchen.bme@gmail.com>
2019-03-06 21:55:30 -0600

    added sim code

---
+++ SimulationCode/EIH_Attenuation_Sim_Main.py
@@ -0,0 +1,49 @@
+from ErgodicHarvestingLib.EIH_API_Sim_Entropy import EIH_Sim
+from scipy.io import loadmat
+from sys import argv
+from numpy import linspace
+from os import getcwd
+from itertools import product
+from multiprocessing import Pool, cpu_count
+# UTC Timestamp
+from time import time
+
+def EID_Attenuation_Sim(simDataFile, nThread): 
+    # Read simulation jobs
+    f = open(simDataFile, 'r')
+    trials = f.readlines()
+    nSimTrials = len(trials)
+
+    # Submit simulations
+    if nThread > cpu_count():
+        nThread = cpu_count()
+    if nSimTrials < nThread:
+        nThread = nSimTrials
+    # Start a new parallel pool  
+    print("Starting parallel pool with {0} threads".format(nThread))
+    pool = Pool(processes=nThread)
+    jobs = []
+    for it in range(nSimTrials):
+        # Wait for all the thread to finish if the pool is full
+        if len(jobs) == nThread:
+            print("All thread used, waiting until finish... ...")
+            # Wait until all the active thread to finish
+            for job in jobs:
+                job.join()
+            
+            print("Simulation Batch done!")
+            # Clear the pool
+            jobs = []
+        
+        # Start a new job thread
+        p = pool.Process(target=EIH_Sim, args=trials[it].split())
+        p.start()
+        jobs.append(p)
+        
+        print("Starting New Simulation Thread ({0:d}/{1:d}), progress {2:.2f}% ({3:d}/{4:d})".format(
+            len(jobs), nThread,
+            100.0*it/nSimTrials, it+1, nSimTrials))
+    print("Waiting for remaining jobs to finish... ...")
+    # Wait until all the active thread to finish
+    for job in jobs:
+        job.join()
---
+++ SimulationCode/RunAllSims.py
@@ -0,0 +1,57 @@
+import sys
+from itertools import product
+from multiprocessing import Pool, cpu_count
+from scipy.io import loadmat, savemat
+from numpy import linspace, arange, concatenate
+from os import makedirs, scandir
+from os.path import exists
+from shutil import copy2
+import numpy as np
+# Suppress all warnings
+np.seterr(all='ignore')
+import warnings
+warnings.filterwarnings("ignore")
+# UTC Timestamp
+import time
+from datetime import datetime
+# Ergodic Harvesting Code
+from ErgodicHarvestingLib.SimParameters import ErgodicParameters, EIDParameters
+from ErgodicHarvestingLib.Simulation import EIDSim
+from EIH_Attenuation_Sim_Main import EID_Attenuation_Sim
+from SimulationMain import SimulationMain
+
+# get number of parallel threads, EAFP way
+try:
+    print(sys.argv[1])
+    nThread = int(sys.argv[1])
+except:
+    nThread = cpu_count() # default
+
+print(f'using {nThread} threads')
+
+timeStampStart0 = datetime.fromtimestamp(time.time())
+params = []
+timeStampStart = datetime.fromtimestamp(time.time())
+
+# load parameter files
+paramPath = './SimParameters/'
+if len(sys.argv) > 3:
+    params.append(paramPath + sys.argv)
+else:
+    for f in scandir(paramPath):
+        if f.is_file() and '.json' in f.name:
+            params.append(paramPath + f.name)
+
+nSimJobs = len(params)
+print(f'Submitting {nSimJobs} total simulation jobs...')
+print('---------------------------------------------------------------')
+SimulationMain(params, nThread=nThread)
+
+# wiggle attenuation simulations 
+#EID_Attenuation_Sim('./SimParameters/SimJobList.txt', nThread)
+
+timeStampEnd = datetime.fromtimestamp(time.time())
+timeString = timeStampEnd.strftime("%b-%d-%Y %T")
+durationMinutes = (timeStampEnd - timeStampStart0).total_seconds() / 60.0
+print("All done! EOF at {0}, total time taken for all simulation(s) {1:.2f} minutes".format(
+    timeString, durationMinutes))
