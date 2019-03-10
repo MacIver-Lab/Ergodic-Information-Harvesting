df2a58938e2751aa0636c710004cc138b654bf96
Chen Chen <chenchen.bme@gmail.com>
2019-03-10 15:28:23 -0500

    Update BuildCython.sh

--- BuildCython.sh
+++ BuildCython.sh
@@ -1,4 +1,2 @@
 cd ./SimulationCode/ErgodicHarvestingLib/
 python ./CythonSetup.py build_ext --inplace
-cd ../../Production-Figure-Code/FigureCode/sm-fig4/ErgodicInfotaxisAPI
-python ./CythonSetup.py build_ext --inplace
