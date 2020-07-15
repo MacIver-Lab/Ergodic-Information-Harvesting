a61644f83900b2f7af06035288294c2046a1056f
Chen Chen <chenchen.bme@gmail.com>
2020-07-15 14:07:00 -0500

    update log check code

--- SimulationCode/check_sim_logs.py
+++ SimulationCode/check_sim_logs.py
@@ -4,5 +4,7 @@ import sys
 if __name__ == "__main__":
     with open(sys.argv[1], 'r') as fp:
         log = fp.read()
-    logs = np.unique([line.split(' ')[-1][:-5] for line in log.split('\n') if 'Received' in line])
-    print(f'Found {len(logs)} total unique sims, expected 938, diff is {938-len(logs)}')
+    logs = [line.split(' ')[-1][:-5] for line in log.split('\n') if 'Received' in line]
+    unique_logs = np.unique(logs)
+    print(f'Found {len(unique_logs)} total unique sims, expected 938, diff is {938-len(unique_logs)}')
+    assert len(logs) == len(unique_logs)
